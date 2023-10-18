af1908444db229b6250d52a12bfcfeac2e54964b
AdelinCZ <adelin.dumbrava@gmail.com>
2023-10-18 18:39:34 +0300

    Modificari Domain

---
+++ Endava.TechCourse.BankApp.Domain/Models/Currency.cs
@@ -0,0 +1,11 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Domain.Models
+{
+    internal class Currency : BaseEntity
+    {
+        public decimal Name { get; set; }
+        public decimal ChangeRate { get; set; }
+        public string CurrencyCode { get; set; }
+    }
+}
---
+++ Endava.TechCourse.BankApp.Domain/Models/Wallet.cs
@@ -0,0 +1,11 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Domain.Models
+{
+    internal class Wallet : BaseEntity
+    {
+        public decimal Amount { get; set; }
+        public Currency Currency { get; set; }
+        public string Type { get; set; }
+    }
+}
