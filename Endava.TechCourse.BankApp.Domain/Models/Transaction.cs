16c0ae20914c3f20657736454f25f8f108b0fd8a
Adelin Dumbrava <adelin.dumbrava@gmail.com>
2023-10-23 11:01:13 +0300

    T-2 Class add

---
+++ Endava.TechCourse.BankApp.Domain/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Shared
+{
+    internal class Transaction : BaseEntity
+    {
+        public int TransactionId { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
--- Endava.TechCourse.BankApp.Domain/Models/Wallet.cs
+++ Endava.TechCourse.BankApp.Domain/Models/Wallet.cs
@@ -4,8 +4,8 @@ namespace Endava.TechCourse.BankApp.Domain.Models
 {
     internal class Wallet : BaseEntity
     {
-        public decimal Amount { get; set; }
-        public Currency Currency { get; set; }
-        public string Type { get; set; }
+        public decimal Name { get; set; }
+        public decimal ChangeRate { get; set; }
+        public string CurrencyCode { get; set; }
     }
 }
