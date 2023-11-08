89d4035c0b9ce0ef635920c29b297924da7a1215
AdelinCZ <adelin.dumbrava@gmail.com>
2023-11-08 17:19:29 +0200

    ADD T-6

---
+++ Endava.TechCourse.BankApp.Application/Commands/DeleteWallet/DeleteWalletHandler.cs
@@ -0,0 +1,32 @@
+﻿using Endava.TechCourse.BankApp.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Endava.TechCourse.BankApp.Application.Commands.DeleteWallet
+{
+    public class DeleteWalletHandler : IRequestHandler<DeleteWalletCommand, CommandStatus>
+    {
+        private readonly ApplicationDbContext context;
+
+        public DeleteWalletHandler(ApplicationDbContext context)
+        {
+            ArgumentNullException.ThrowIfNull(context);
+            this.context = context;
+        }
+
+        public async Task<CommandStatus> Handle(DeleteWalletCommand request, CancellationToken cancellationToken)
+        {
+            var id = Guid.Parse(request.WalletId);
+
+            var wallet = await context.Wallets.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+            if (wallet is null)
+                return CommandStatus.Failed($"Nu exista wallet cu id ul {request.WalletId}");
+
+            context.Wallets.Remove(wallet);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return new();
+        }
+    }
+}
---
+++ Endava.TechCourse.BankApp.Application/Queries/GetWalletById/GetWalletByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿using Endava.TechCourse.BankApp.Domain.Models;
+using Endava.TechCourse.BankApp.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Endava.TechCourse.BankApp.Application.Queries.GetWalletById
+{
+    public class GetWalletByIdHandler : IRequestHandler<GetWalletByIdQuery, List<Wallet>>
+    {
+        private readonly ApplicationDbContext context;
+
+        public GetWalletByIdHandler(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Wallet>> Handle(GetWalletByIdQuery request, CancellationToken cancellationToken)
+        {
+            var wallet = await this.context.Wallets.Include(x => x.Id).ToListAsync(cancellationToken);
+            return wallet;
+        }
+    }
+}
